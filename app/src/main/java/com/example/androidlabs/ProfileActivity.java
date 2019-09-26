609bb23cdf6abfc3ee350b65a53ebfeebc4a65b4
ximenxiao <sijie18@gmail.com>
2019-09-26 16:11:42 -0400

    Added 2 layouts for Lab 3 annd functions

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -1,52 +1,49 @@
 package com.example.androidlabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main_linear);
-       //setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_relative);
         //setContentView(R.layout.activity_main_grid);
-        setContentView(R.layout.activity_email);
+        //setContentView(R.layout.activity_second);
 
-        if(loginButton != null)
-            loginButton.setOnClickListener(v -> {
-                Intent goToPage2 = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(goToPage2);
-            });
+
 
 
 
     }
 
-    EditText editText = findViewById(R.id.emailText);
+    /*EditText editText = findViewById(R.id.emailText);
     SharedPreferences prefs = getSharedPreferences("FileName", MODE_PRIVATE);
-    Button loginButton = findViewById(R.id.loginButton);
     @Override
     protected void onPause() {
         super.onPause();
-      //  EditText editText = findViewById(R.id.emailText);
+        //  EditText editText = findViewById(R.id.emailText);
+        Button loginButton = findViewById(R.id.loginButton);
         //SharedPreferences prefs = getSharedPreferences("FileName", MODE_PRIVATE);
         String previous = prefs.getString("ReserveEmail", "Default Value");
         editText.setText(previous);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View e) {
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putString("ReserveEmail", editText.getText().toString());
+
                 editor.commit();
             }
         });
 
-    }
+    }*/
 }
